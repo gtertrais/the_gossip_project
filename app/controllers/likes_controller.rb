cff9753197fc7d4d72ce898c3aaec98ebf8094be
Gaspard49 <gaspard.tertrais@gmail.com>
2019-08-08 18:09:24 +0000

    start likes

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+class LikesController < ApplicationController
+  before_action :require_login, only: %i[new create edit destroy]
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(content: params[:content], user: User.find_by(id: session[:user_id]), gossip_id: params[:gossip_id])
+    if @comment.save
+      flash[:notice] = 'Comment was successfully created.'
+      redirect_to(@comment.gossip)
+    else
+      flash[:notice] = "Error creating comment: #{@comment.errors}"
+      redirect_to gossips_path
+    end
+    end
+
+  def destroy
+    @gossip = Gossip.find(params[:gossip_id])
+    @comment = @gossip.comments.find(params[:id])
+    @comment.destroy
+    redirect_to @gossip
+  end
+
+  def edit
+    @gossip = Gossip.find(params[:gossip_id])
+    @comment = @gossip.comments.find(params[:id])
+  end
+
+  def update
+    @gossip = Gossip.find(params[:gossip_id])
+    @comment = @gossip.comments.find(params[:id])
+
+    if @comment.update(comment_params)
+      flash[:success] = 'The Comment was successfully edited'
+      redirect_to @gossip
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+
+  def require_login
+    unless session[:user_id]
+      flash[:error] = 'You must be logged in to access this section'
+      redirect_to new_session_path # halts request cycle
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'sessions/new'
   resources :gossips do
     resources :comments
+    resources :likes
   end
   resources :contact
   resources :team
